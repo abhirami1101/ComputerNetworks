77a610cf32f1cabb484361e4e1e677770eaee929
abhirami1101 <112201001@smail.iitpkd.ac.in>
2025-10-26 11:17:59 +0530

    added Proxy server

---
+++ Server.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+import sys
+
+class ProxyServer:
+
+    def __init__(self, port = 8080):
+        self.host = '0.0.0.0'
+        self.port = port
+        self.running = True
+        self.server_socket = None
+
+    def start(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(100)
+        print(f"Proxy server started on port {self.port}")
+
+        while self.running:
+            client_socket, addr = self.server_socket.accept()
+            print(f"Accepted connection from {addr}")
+            # start a new thread to handle the client connection
+            client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
+            client_handler.start()
+        
+    def close(self):
+        self.running = False
+        if self.server_socket:
+            self.server_socket.close()
+            print("Proxy server closed")
+
+    
